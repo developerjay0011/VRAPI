877f26332cd0a483c1f70f59fa2b6ef667e1e668
Jay Dhimar <jaydhimar1@gmail.com>
2025-03-26 11:37:09 +0530

    comiit code

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'loan_landing_page',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
+  process.env.DB_NAME || 'vr_consultancy',
+  process.env.DB_USER || 'admin',
+  process.env.DB_PASSWORD || 'Karan@8055',
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '91.108.104.208',
     dialect: 'mysql',
     logging: false,
   }
